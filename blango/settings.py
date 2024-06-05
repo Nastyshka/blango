72b1a9b071c3125d39f5dd6dd83f0a2d3790b82f
Anastasiia Rudyk <nastya.rudyk@gmail.com>
2024-06-05 22:08:56 +0000

    Finish django Allauth

--- blango/settings.py
+++ blango/settings.py
@@ -21,7 +21,12 @@ class Dev(Configuration):
 
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
     ACCOUNT_ACTIVATION_DAYS = 7
-    
+    SITE_ID = 1
+    ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+    ACCOUNT_EMAIL_REQUIRED = True
+    ACCOUNT_USERNAME_REQUIRED = False
+    ACCOUNT_AUTHENTICATION_METHOD = "email"
+
     # Application definition
     INSTALLED_APPS = [
         'django.contrib.admin',
@@ -29,12 +34,17 @@ class Dev(Configuration):
         'django.contrib.contenttypes',  # Ensure this line is present
         'django.contrib.sessions',
         'django.contrib.messages',
+        'django.contrib.sites',
         'django.contrib.staticfiles',
         'crispy_forms',
         'crispy_bootstrap5',
         'debug_toolbar',
         'blango_auth',
         'blog',
+        "allauth",
+        "allauth.account", 
+        "allauth.socialaccount", 
+        "allauth.socialaccount.providers.google"
     ]
 
     MIDDLEWARE = [
